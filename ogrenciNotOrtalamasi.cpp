7c1f2006830673b81bb4c1b9c064b86f00be9d2f
Sudenur <temucinsudenur@gmail.com>
2025-07-26 20:09:03 +0300

    ogrenciNotOrtalamasi.cpp eklendi

---
+++ ogrenciNotOrtalamasi.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int not1;
+	int not2;
+	int not3;
+	double notOrtalamasi;
+
+	cout << "Lutfen 3 adet sinav notu giriniz: " << endl;
+	cin >> not1 >> not2 >> not3;
+
+
+	notOrtalamasi = (double) (not1 + not2 + not3 ) / 3.0;
+
+	//1) Ondalıklı şekilde iki basamak hassasiyetli yazdırmak:
+	cout << "1) Not Ortalamasi(ondalikli): " << fixed << setprecision(2) << notOrtalamasi << endl;
+
+	//2) Yuvarlama yaparak tamsayı sonuç:
+	int sonuc = (int) round(notOrtalamasi);
+	cout << "2) Not Ortalamasi(tam sayi , yuvarlamali): " << sonuc << endl;
+
+	return 0;
+}
